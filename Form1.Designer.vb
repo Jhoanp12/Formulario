eb7e40b54228a5a5cb80ea1bee7a4ab37da53016
uestudiantes <uestudiantes@ESTUDIANT-A27>
2022-04-22 21:20:45 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.txtUser = New System.Windows.Forms.TextBox()
+        Me.txtPass = New System.Windows.Forms.TextBox()
+        Me.BtnSend = New System.Windows.Forms.Button()
+        Me.lblUser = New System.Windows.Forms.Label()
+        Me.lblPassword = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
+        Me.lblTitle.ForeColor = System.Drawing.SystemColors.MenuHighlight
+        Me.lblTitle.Location = New System.Drawing.Point(214, 41)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(118, 46)
+        Me.lblTitle.TabIndex = 0
+        Me.lblTitle.Text = "Login"
+        '
+        'txtUser
+        '
+        Me.txtUser.Location = New System.Drawing.Point(280, 130)
+        Me.txtUser.Name = "txtUser"
+        Me.txtUser.Size = New System.Drawing.Size(202, 26)
+        Me.txtUser.TabIndex = 1
+        '
+        'txtPass
+        '
+        Me.txtPass.Location = New System.Drawing.Point(280, 194)
+        Me.txtPass.Name = "txtPass"
+        Me.txtPass.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPass.Size = New System.Drawing.Size(202, 26)
+        Me.txtPass.TabIndex = 2
+        '
+        'BtnSend
+        '
+        Me.BtnSend.Location = New System.Drawing.Point(164, 307)
+        Me.BtnSend.Name = "BtnSend"
+        Me.BtnSend.Size = New System.Drawing.Size(212, 51)
+        Me.BtnSend.TabIndex = 3
+        Me.BtnSend.Text = "Send"
+        Me.BtnSend.UseVisualStyleBackColor = True
+        '
+        'lblUser
+        '
+        Me.lblUser.AutoSize = True
+        Me.lblUser.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.lblUser.Location = New System.Drawing.Point(63, 130)
+        Me.lblUser.Name = "lblUser"
+        Me.lblUser.Size = New System.Drawing.Size(64, 29)
+        Me.lblUser.TabIndex = 4
+        Me.lblUser.Text = "User"
+        '
+        'lblPassword
+        '
+        Me.lblPassword.AutoSize = True
+        Me.lblPassword.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.lblPassword.Location = New System.Drawing.Point(63, 194)
+        Me.lblPassword.Name = "lblPassword"
+        Me.lblPassword.Size = New System.Drawing.Size(120, 29)
+        Me.lblPassword.TabIndex = 5
+        Me.lblPassword.Text = "Password"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(563, 436)
+        Me.Controls.Add(Me.lblPassword)
+        Me.Controls.Add(Me.lblUser)
+        Me.Controls.Add(Me.BtnSend)
+        Me.Controls.Add(Me.txtPass)
+        Me.Controls.Add(Me.txtUser)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Name = "Form1"
+        Me.Text = "Inventario"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents txtUser As TextBox
+    Friend WithEvents txtPass As TextBox
+    Friend WithEvents BtnSend As Button
+    Friend WithEvents lblUser As Label
+    Friend WithEvents lblPassword As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblTitle.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles BtnSend.Click
+
+        Dim user As String
+        Dim pass As String
+        user = txtUser.Text
+        pass = txtPass.Text
+
+        'MsgBox("User: " & user)
+
+    End Sub
+
+    Private Sub Label2_Click_1(sender As Object, e As EventArgs) Handles lblPassword.Click
+
+    End Sub
+
+    Private Sub lblUser_Click(sender As Object, e As EventArgs) Handles lblUser.Click
+
+    End Sub
+
+    Private Sub txtUser_TextChanged(sender As Object, e As EventArgs) Handles txtUser.TextChanged
+
+    End Sub
+
+    Private Sub txtPass_TextChanged(sender As Object, e As EventArgs) Handles txtPass.TextChanged
+
+    End Sub
+
+
+End Class
